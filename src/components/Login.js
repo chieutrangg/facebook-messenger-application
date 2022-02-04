44394914c4a67fb5f78fd7994aa156e2e9a5c347
chieutrangg <chieutrangg@gmail.com>
2022-02-04 19:58:35 +0200

    fixed problem with firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,9 @@ import React from "react"
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons"
 import "firebase/app"
 import { auth } from "../firebase"
+import firebase from "firebase/app"
+
+
 const Login = () => {
     return (
         <div id="login-page">
