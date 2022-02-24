9f83ae9e243887edf6a5675d7f718371f45bc831
chieutrangg <chieutrangg@gmail.com>
2022-02-25 01:08:58 +0200

    Updates

---
+++ precache-manifest.8288537d2859be6f7ff71bb3e7a33426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425341a26e785aa430f97af31074ebbc",
+    "url": "/real-time-chat-messaging-app/index.html"
+  },
+  {
+    "revision": "3300aa96f3c5d4b675b1",
+    "url": "/real-time-chat-messaging-app/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "9d259e0ae2291e714fd8",
+    "url": "/real-time-chat-messaging-app/static/js/2.27607621.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/real-time-chat-messaging-app/static/js/2.27607621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3300aa96f3c5d4b675b1",
+    "url": "/real-time-chat-messaging-app/static/js/main.3f730ba3.chunk.js"
+  },
+  {
+    "revision": "3c1cd087af62b076f6e8",
+    "url": "/real-time-chat-messaging-app/static/js/runtime-main.c7aa998a.js"
+  }
+]);
